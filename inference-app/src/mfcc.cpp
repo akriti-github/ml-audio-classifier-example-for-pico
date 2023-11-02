45209746df4ad094f75ad07482c3c2f1fa55afd6
akriti-github <82258844+akriti-github@users.noreply.github.com>
2023-11-02 18:10:39 +0530

    Update mfcc.cpp

--- inference-app/src/mfcc.cpp
+++ inference-app/src/mfcc.cpp
@@ -1,4 +1,145 @@
 #include "mfcc.h"
+def preemphasis(xn, alpha = 0.97):
+    # Using pre-empaphases with a certain alpha
+    pre_e_xn = np.zeros((xn.shape))
+    
+    pre_e_xn[0] = xn[0]
+    
+    pre_e_xn[1:] = xn[1:] - alpha * xn[:-1]
+    
+    return pre_e_xn
+
+def get_mel_from_hertz(hertz):
+    return 2595 * np.log10(1 + (hertz/ 700))
+
+def get_hertz_from_mel(mel):
+    return 700 * (10**(mel / 2595) - 1)
+
+def get_power_spectrum(xn_mag, fft_size=2048):
+    return (1/fft_size) * np.power(xn_mag, 2)
+
+def get_triangle_function(prev_freq, cur_freq, nex_freq, filter_banks, bin_fb):
+    
+    # Ascending Triangle
+    
+    for freq in range(int(prev_freq), int(cur_freq)):
+        
+        filter_banks[bin_fb-1,freq] = (freq - prev_freq)/(cur_freq-prev_freq)
+        
+    # Descending Triangle
+    
+    for freq in range(int(cur_freq+1), int(nex_freq)):
+        
+        filter_banks[bin_fb-1, freq] = (nex_freq-freq)/(nex_freq-cur_freq)
+        
+    # Triangle Tip
+    
+    filter_banks[bin_fb-1, int(cur_freq)] = 1
+    
+    return filter_banks
+
+def mel_filter_banks(xn_pow, sr, number_filters, fft_size=2048):
+    min_mel = 0
+    max_mel = get_mel_from_hertz(sr/2)
+    
+    mel_freq_points = np.linspace(min_mel, max_mel, num=number_filters+2)
+    hertz_freq_points = get_hertz_from_mel(mel_freq_points)
+    
+    corresponding_bins_hertz_points = np.floor((fft_size + 1) * hertz_freq_points / sr)
+    
+    # Filter banks have to be of shape number_filters * (fft_size/2) + 1
+    filter_banks = np.zeros((number_filters, int(fft_size/2)+1))
+    
+    for bin_fb in range(1, number_filters+1):
+        
+        prev_bin = corresponding_bins_hertz_points[bin_fb-1]
+        current_bin = corresponding_bins_hertz_points[bin_fb]
+        next_bin = corresponding_bins_hertz_points[bin_fb+1]
+        
+        # Use the triangle function to get the values of the banks
+        
+        filter_banks = get_triangle_function(prev_bin, current_bin, next_bin, filter_banks, bin_fb)
+        
+    return filter_banks
+
+def get_delta_values(x):
+    delta_x = np.zeros(shape=x.shape)
+    for i in range(1,x.shape[1]-1):
+        prev_val = x[:,i-1]
+        next_val = x[:,i+1]
+        
+        delta_x[:,i]  = (next_val - prev_val)/2
+    
+    return delta_x
+
+def mfcc(xn, sr, number_filters, window_size = 500, hopsize=int(500/4), fft_size=512):
+    
+    # Pre-emphasis
+    
+    xn = preemphasis(xn)
+    
+    # Getting the STFT
+        
+    xn_stft = stft(xn, window_size= window_size, hopsize=hopsize, fft_size=fft_size)
+    
+    # Getting the Magnitude of the STFT
+    
+    xn_mag = np.abs(xn_stft)
+    
+    # Evaluating the Power spectrum for the magnitude
+    
+    xn_pow = get_power_spectrum(xn_mag, fft_size=fft_size)
+    
+    # To get the mel filter banks
+    
+    filter_banks = mel_filter_banks(xn_pow, sr, number_filters, fft_size=fft_size)
+    
+    machine_epsilon =  2.22044604925e-16
+    
+    filter_banks[filter_banks==0] = machine_epsilon
+    
+    
+    # Multiply the filter_banks with the power spectrum
+    
+    filter_banks_res = np.dot(filter_banks, xn_pow.T)
+    
+    # Taking the log and the inverse DFT
+    
+    filter_banks_res = filter_banks_res + machine_epsilon
+    
+    log_filter_bank = np.log(filter_banks_res)
+    
+    idft = sp.fftpack.dct(log_filter_bank)
+    
+    # First 12 MFCC Values
+    
+    first_12 = idft[:12,:]
+    
+    # delta and delta-delta coefficients
+    
+    delta = get_delta_values(idft)
+    
+    delta_delta = get_delta_values(delta)
+    
+    # Getting Energy values of delta and delta-delta coefficients
+    
+    first_12_delta = delta[:12,:]
+    
+    first_12_delta_delta = delta_delta[:12,:]
+    
+    
+    # Energy of the Cepstrum frame. Read from - http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.596.8754&rep=rep1&type=pdf
+    
+    energy = np.sqrt(np.sum(np.power(first_12,2),axis=0)).reshape(1,-1)
+    
+    energy_delta = np.sqrt(np.sum(np.power(first_12_delta,2),axis=0)).reshape(1,-1)
+    
+    energy_delta_delta = np.sqrt(np.sum(np.power(first_12_delta_delta,2),axis=0)).reshape(1,-1)
+    
+    return np.vstack((energy, energy_delta, energy_delta_delta, first_12, first_12_delta, first_12_delta_delta)), filter_banks
+
+mfcc_xn, filter_banks = mfcc(xn, sr, 40)
+
 
 MFCC::MFCC(int num_mfcc_coeffs, int frame_size, int num_fft_points) :
     _num_mfcc_coeffs(num_mfcc_coeffs),
